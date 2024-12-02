55a5e049d2ed5d7b6833ea2883dbe16fadbd0a8e
许远航 <1223050251@qq.com>
2024-12-02 17:39:07 +0800

    自定义后台登录页面（不修改vendor）

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -2,8 +2,38 @@
 
 namespace App\Admin\Controllers;
 
+use Illuminate\Support\Facades\Validator;
+use Dcat\Admin\Layout\Content;
+use Illuminate\Http\Request;
 use Dcat\Admin\Http\Controllers\AuthController as BaseAuthController;
 
-class AuthController extends BaseAuthController
-{
+class AuthController extends BaseAuthController{
+
+    protected $view = 'admin.login';
+
+    public function getLogin(Content $content){
+        if ($this->guard()->check()) {
+            return redirect($this->getRedirectPath());
+        }
+
+        return $content->full()->body(view($this->view));
+    }
+
+    /**
+     * Handle a login request.
+     *
+     * @param  Request  $request
+     * @return mixed
+     */
+    public function postLogin(Request $request){
+        // 验证验证码
+        $captcha = $request->input('captcha', '');
+        $storedCaptcha = $request->input('captcha_code', '');
+        if(!hash_equals(strtolower($captcha), strtolower($storedCaptcha))){
+            return $this->validationErrorsResponse([
+                'captcha' => "验证码输入错误",
+            ]);
+        }
+        return parent::postLogin($request);
+    }
 }
