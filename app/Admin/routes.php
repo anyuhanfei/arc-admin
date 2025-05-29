68c21f8c38ac41299d9c89de36bdc0c88dca3655
anyuhanfei <anyuhanfei@gmail.com>
2025-05-29 15:00:44 +0800

    修改后台路由格式，改为编辑器可跳转的格式

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -4,6 +4,13 @@ use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use Dcat\Admin\Admin;
 
+use App\Admin\Controllers\Sys;
+use App\Admin\Controllers\Users;
+use App\Admin\Controllers\Article;
+use App\Admin\Controllers\AuthController;
+use App\Admin\Controllers\Feedback;
+use App\Admin\Controllers\Faqs;
+
 Admin::routes();
 
 Route::group([
@@ -14,22 +21,22 @@ Route::group([
 
     $router->get('/', 'HomeController@index');
 
-    $router->resource('sys/banners', 'Sys\SysBannersController');
-    $router->resource('sys/settings/set', 'Sys\SysSettingsController');
-    $router->resource('sys/settings', 'Sys\SysSettingsController');
-    $router->resource('sys/notices', 'Sys\SysNoticesController');
-    $router->resource('sys/messages', "Sys\SysMessageLogsController");
+    $router->resource('sys/banners', Sys\SysBannersController::class);
+    $router->resource('sys/settings/set', Sys\SysSettingsController::class);
+    $router->resource('sys/settings', Sys\SysSettingsController::class);
+    $router->resource('sys/notices', Sys\SysNoticesController::class);
+    $router->resource('sys/messages', Sys\SysMessageLogsController::class);
 
-    $router->resource('users/users', 'Users\UsersController');
-    $router->resource('log/balances', 'Users\UserBalanceLogsController');
-    $router->resource('log/withdraws', 'Users\UserWithdrawLogsController');
+    $router->resource('users/users', Users\UsersController::class);
+    $router->resource('log/balances', Users\UserBalanceLogsController::class);
+    $router->resource('log/withdraws', Users\UserWithdrawLogsController::class);
 
-    $router->resource('article/articles', 'Article\ArticlesController');
-    $router->resource('article/categories', 'Article\ArticleCategoriesController');
-    $router->resource('feedback/types', 'Feedback\FeedbackTypesController');
-    $router->resource('feedback/feedbacks', 'Feedback\FeedbacksController');
-    $router->resource('faqs/types', 'Faqs\FaqTypesController');
-    $router->resource('faqs/faqs', 'Faqs\FaqsController');
+    $router->resource('article/articles', Article\ArticlesController::class);
+    $router->resource('article/categories', Article\ArticleCategoriesController::class);
+    $router->resource('feedback/types', Feedback\FeedbackTypesController::class);
+    $router->resource('feedback/feedbacks', Feedback\FeedbacksController::class);
+    $router->resource('faqs/types', Faqs\FaqTypesController::class);
+    $router->resource('faqs/faqs', Faqs\FaqsController::class);
 
 
 });
@@ -37,9 +44,9 @@ Route::group([
     'prefix'     => config('admin.route.prefix'),
     'namespace'  => config('admin.route.namespace'),
 ], function (Router $router) {
-    Route::get("captcha/image", "AuthController@captcha_image");
-    Route::get("/get/articles/list", "Article\ArticlesController@get_articles_list");
-    Route::get("get/article/categories", "Article\ArticleCategoriesController@get_categories");
-    Route::get("get/users", "Users\UsersController@get_users");
-    Route::get("get/faqs/types", "Faqs\FaqTypesController@get_types");
+    Route::get("captcha/image", [AuthController::class, 'captcha_image']);
+    Route::get("/get/articles/list", [Article\ArticlesController::class, 'get_articles_list']);
+    Route::get("get/article/categories", [Article\ArticleCategoriesController::class, 'get_categories']);
+    Route::get("get/users", [Users\UsersController::class, 'get_users']);
+    Route::get("get/faqs/types", [Faqs\FaqTypesController::class, 'get_types']);
 });
